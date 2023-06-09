4b11076a202cfd27b742443c8fd9e6f788b993e6
Tilak Puli <tilakpuli15@gmail.com>
2023-06-09 15:01:09 +0530

    feat[#2]: update commit zen config

--- .czferc.js
+++ .czferc.js
@@ -7,10 +7,14 @@ const {types} = require('./commit_types.json');
 const issueRegEx = /^((fix)|(re))\s\#[1-9][0-9]{0,4}$/g;
 const pairRegEx = /^\w{3,15}(\/\w{3,15}){1,2}$/g;
 const alphaNumericRegEx = /^[a-zA-Z0-9_ ]*$/g;
+const numRegEx = /^[0-9]*$/g;
 const longMessageRegEx = /^[a-zA-Z\d-_\/\(\) ]+$/g;
 
 const alphaNumericValidator = value =>
   value.match(alphaNumericRegEx) ? true : 'input should be alphanumeric only';
+
+const numericValidator = value =>
+  value.match(numRegEx) ? true : 'issue should be numeric only';
 
 const longMessageValidator = value =>
   value
@@ -33,12 +37,20 @@ module.exports = {
         message: "Select the type of change that you're committing",
         choices: types,
       },
+      // {
+      //   type: 'input',
+      //   name: 'scope',
+      //   message:
+      //     'What is the scope of this change (e.g. component or file name): (press enter to skip)\n',
+      //   validate: alphaNumericValidator,
+      // },
       {
         type: 'input',
-        name: 'scope',
-        message:
-          'What is the scope of this change (e.g. component or file name): (press enter to skip)\n',
-        validate: alphaNumericValidator,
+        name: 'issues',
+        message: 'Add issue references:\n',
+        // when: answers => answers.isIssueAffected,
+        default: undefined,
+        validate: numericValidator,
       },
       {
         type: 'input',
@@ -67,12 +79,12 @@ module.exports = {
           'Provide a longer description of the change: (press enter to skip)\n',
         validate: longMessageValidator,
       },
-      {
-        type: 'confirm',
-        name: 'isBreaking',
-        message: 'Are there any breaking changes?',
-        default: false,
-      },
+      // {
+      //   type: 'confirm',
+      //   name: 'isBreaking',
+      //   message: 'Are there any breaking changes?',
+      //   default: false,
+      // },
       {
         type: 'input',
         name: 'breakingBody',
@@ -88,12 +100,12 @@ module.exports = {
         message: 'Describe the breaking changes:\n',
         when: answers => answers.isBreaking,
       },
-      {
-        type: 'confirm',
-        name: 'isIssueAffected',
-        message: 'Does this change affect any open issues?',
-        default: false,
-      },
+      // {
+      //   type: 'confirm',
+      //   name: 'isIssueAffected',
+      //   message: 'Does this change affect any open issues?',
+      //   default: false,
+      // },
       {
         type: 'input',
         name: 'issuesBody',
@@ -104,31 +116,17 @@ module.exports = {
           answers.isIssueAffected && !answers.body && !answers.breakingBody,
         validate: longMessageValidator,
       },
-      {
-        type: 'input',
-        name: 'issues',
-        message: 'Add issue references (e.g. "fix #123", "re #123".):\n',
-        when: answers => answers.isIssueAffected,
-        default: undefined,
-        validate: issues =>
-          issues.length === 0
-            ? 'issues is required'
-            : issues.match(issueRegEx)
-            ? true
-            : 'invalid issue format',
-      },
     ];
   },
   commitMessage({answers}) {
-    const scope = answers.scope ? `(${answers.scope})` : '';
     const pair = answers.pair ? `[${answers.pair}]` : '';
-    const head = `${answers.type}${scope}:${pair} ${answers.subject}`;
     const body = answers.body ? answers.body : '';
     const breaking = answers.breaking
       ? `BREAKING CHANGE: ${answers.breaking}`
       : '';
-    const issues = answers.issues ? answers.issues : '';
+    const issues = answers.issues ? `[#${answers.issues}]` : '';
+    const head = `${answers.type}${issues}:${pair} ${answers.subject}`;
 
-    return [head, body, breaking, issues].join('\n\n').trim();
+    return [head, body, breaking].join('\n\n').trim();
   },
 };
